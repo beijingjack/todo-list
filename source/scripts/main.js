a3a605d625797c6fcf76b5536b34e93bd5ab5ab8
beijingjack <lijingyu951126@gmail.com>
2019-05-06 20:17:44 -0400

    done

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+const handleSubmit = (e) => {
+  let currTask;
+  let inputVal = document.getElementById("input-bar").value;
+  if (inputVal.length !== 0) {
+    document.getElementById("input-bar").value = "";
+    currTask = inputVal;
+    let ul = document.getElementById("task-list");
+    let li = document.createElement("li");
+    li.className = "item";
+    let span1 = document.createElement("SPAN");
+    span1.className = "task-name";
+    span1.appendChild(document.createTextNode(`${currTask}`));
+    li.appendChild(span1);
+    let span2 = document.createElement("SPAN");
+    span2.className = "close";
+    let txt = document.createTextNode("\u00D7");
+    span2.appendChild(txt);
+    span2.onclick = function () {
+      this.parentElement.style.display = "none";
+    };
+    li.onclick = function () {
+      this.style.backgroundColor = "#888"
+      this.firstChild.style.textDecoration = "line-through";
+    };
+    li.appendChild(span2);
+    ul.appendChild(li);
+  }
+};
+
+document.getElementById("submit-button").addEventListener("click", handleSubmit);
